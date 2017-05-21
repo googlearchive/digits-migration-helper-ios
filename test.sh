f430b870910070ddb28ac0509e571f2402b354fa
Ibrahim Ulukaya <ulukaya@google.com>
2017-05-21 15:01:57 -0400

    update Travis settings

---
+++ test.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -o pipefail && xcodebuild \
+  -workspace Example/DigitsMigrationHelper.xcworkspace \
+  -scheme DigitsMigrationHelper-Example \
+  -sdk iphonesimulator \
+  -destination 'platform=iOS Simulator,name=iPhone 7' \
+  build \
+  test \
+  ONLY_ACTIVE_ARCH=YES \
+  CODE_SIGNING_REQUIRED=NO\
+  | xcpretty
+  
+RESULT=$?
+if [ $RESULT == 65 ]; then
+  echo "xcodebuild exited with 65, retrying"
+  set -o pipefail && xcodebuild \
+    -workspace Example/DigitsMigrationHelper.xcworkspace \
+    -scheme DigitsMigrationHelper-Example \
+    -sdk iphonesimulator \
+    -destination 'platform=iOS Simulator,name=iPhone 7' \
+    build \
+    test \
+    ONLY_ACTIVE_ARCH=YES \
+    CODE_SIGNING_REQUIRED=NO\
+    | xcpretty
+else
+  exit $RESULT
+fi
+
